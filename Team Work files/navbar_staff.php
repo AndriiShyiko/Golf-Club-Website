84e6be4c806aab95290d847275ce3e8dac7a3972
Andrii Shyiko <andriishiiko@gmail.com>
2026-03-30 10:55:00 +0100

    Add files via upload

---
+++ Team Work files/navbar_staff.php
@@ -0,0 +1,15 @@
+<!--
+Developed by: Andrii Shyiko
+Date: 01/03/2026
+Purpose: Navigation bar through the staff management 
+Screen Name: Navigation bar
+-->
+
+<div class="navbar">
+    <ul class="navbar__list">
+        <li class="menu__item"><a href="AddStaff.php">Add Staff</a></li>
+        <li class="menu__item"><a href="DeleteStaff.php">Delete Staff</a></li>
+        <li class="menu__item"><a href="AmendView.php">Amend/View Staff</a></li>
+        <li class="menu__item"><a href="PlaceOrder.php">Place Order</a></li>
+    </ul>
+</div>
