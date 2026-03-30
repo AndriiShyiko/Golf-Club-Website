353b8f00144966c44e30a565495c1ec5c0e5a794
Andrii Shyiko <andriishiiko@gmail.com>
2026-03-30 10:53:10 +0100

    Add files via upload

---
+++ Team Work files/sidebar_stock.php
@@ -0,0 +1,15 @@
+<!--
+Developed by: Andrii Shyiko
+Date: 01/03/2026
+Purpose: Sidebar menu for the website
+Screen Name: Sidebar menu
+-->
+<!-- Navigation Sidebar -->
+<div class="sidebar">
+    <div class="logo__image"><a class="pegasus_golf_link" href="index.php"><img src="pegasus_gc_logo.png"></a></div>
+<!--    links to other pages -->
+    <a href="../Andrii Shyiko/AddStaff.php" class="">Staff</a>
+    <a href="" class="">Players</a>
+    <a href="" class="">Competitions</a>
+    <a href="" class="">Stock</a>
+</div>
